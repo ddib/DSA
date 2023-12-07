acf3ceca96d205dab0909fbfe55762fbeb0706f1
ddib <d.djawida@gmail.com>
2023-12-07 19:13:06 +0100

    Create SelectionSort.py
    Selection Sort function 
    Input: Array of comparables
    Output: Sorted array 
    Complexity:
     * Time: O(n²)
     * Space: O(1) 
    Properties:
     * In-place
     * Not stable

---
+++ Sorting/Basics/SelectionSort.py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+    """
+    Args:
+     arr(list_int32)
+    Returns:
+     list_int32
+    """
+    n = len(arr)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
